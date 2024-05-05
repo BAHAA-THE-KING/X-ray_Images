9b53788084f1a7f71cf39a81a0dd645ac975d870
abdulrahman-kharzoum <aboodkh920@gmail.com>
2024-05-05 14:30:09 +0300

    image box

--- Form1.cs
+++ Form1.cs
@@ -1,9 +1,39 @@
-namespace X_ray_Images;
+﻿
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
 
-public partial class Form1 : Form
+using System.Windows.Forms;
+
+
+namespace X_ray_Images
 {
-    public Form1()
+    public partial class Form1 : Form
     {
-        InitializeComponent();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void select_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Image<Bgr, Byte> img = new Image<Bgr, Byte>(openFileDialog1.FileName);
+
+                pictureBox1.Image = img.ToBitmap();
+            }
+        }
     }
 }
