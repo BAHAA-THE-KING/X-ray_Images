958a50e42ba884f9376a268140f71eed3487345b
BAHAA-THE-KING <bahaaalnokta2003@gmail.com>
2024-05-30 01:21:10 +0300

    Fixes.

--- Views/Heart/Heart.cs
+++ Views/Heart/Heart.cs
@@ -13,94 +13,150 @@ namespace X_ray_Images
 	public partial class Heart : Form
 	{
 		HeartMode mode = HeartMode.None;
-		private Point startPoint = new Point(-1, -1);
-		private Rectangle selectionRect = new Rectangle(0, 0, 0, 0);
+		Point startPoint = new Point(-1, -1);
+		Rectangle tempRect = new Rectangle(0, 0, 0, 0);
+		// Coloring Icons
+		void ActiveImage(PictureBox pictureBox)
+		{
+			pictureBox.BackColor = SystemColors.ActiveCaption;
+		}
+		void InactiveImage(PictureBox pictureBox)
+		{
+			pictureBox.BackColor = SystemColors.Control;
+		}
 
 		public Heart()
 		{
 			InitializeComponent();
 		}
 
+		// Management
+		private void ResetState()
+		{
+			if (mode == HeartMode.Select)
+			{
+				Reset();
+				mode = HeartMode.None;
+				InactiveImage(SelectImage);
+			}
+		}
 		private void Reset()
 		{
 			startPoint = new Point(-1, -1);
-			selectionRect = new Rectangle(0, 0, 0, 0);
+			tempRect = new Rectangle(0, 0, 0, 0);
 			MainImage.Invalidate();
 		}
+		private void SetImage(Image image)
+		{
+			if (image == null) return;
+			MainImage.Image = image;
+			MainImage.Size = new Size(image.Width, image.Height);
+		}
 
+		// MainImage Control
 		private void MainImage_MouseDown(object sender, MouseEventArgs e)
 		{
-			if (e.Button == MouseButtons.Right)
+			if (MainImage.Image != null)
 			{
-				Reset();
-				return;
+				if (mode == HeartMode.Select)
+				{
+					if (e.Button == MouseButtons.Right)
+					{
+						Reset();
+						return;
+					}
+					startPoint = Selector.BeginSelect(MainImage, e.X, e.Y);
+					tempRect = new Rectangle(startPoint.X, startPoint.Y, 0, 0);
+				}
 			}
-			startPoint = Selector.BeginSelect(MainImage, e.X, e.Y);
 		}
-
 		private void MainImage_MouseMove(object sender, MouseEventArgs e)
 		{
-			if (e.Button == MouseButtons.Left && startPoint.X != -1)
+			if (MainImage.Image != null)
 			{
-				selectionRect = Selector.SelectMove(MainImage, startPoint, e.X, e.Y);
-				MainImage.Invalidate();
-			}
-			else if (e.Button == MouseButtons.Right)
-			{
-				Reset();
+				if (mode == HeartMode.Select)
+				{
+					if (e.Button == MouseButtons.Left)
+					{
+						if (startPoint.X != -1)
+						{
+							tempRect = Selector.SelectMove(MainImage, startPoint, e.X, e.Y);
+						}
+
+						MainImage.Invalidate();
+					}
+					else if (e.Button == MouseButtons.Right)
+					{
+						Reset();
+					}
+				}
 			}
 		}
-
-		private void MainImage_Paint(object sender, PaintEventArgs e)
+		private void MainImage_Paint(object sender, EventArgs e)
 		{
-			if (selectionRect.Width > 0 && selectionRect.Height > 0)
+			if (MainImage.Image != null)
 			{
-				e.Graphics.DrawRectangle(Pens.Red, selectionRect);
+				PaintEventArgs paintEvent = (PaintEventArgs)e;
+				if (tempRect.Width > 0 && tempRect.Height > 0)
+				{
+					paintEvent.Graphics.DrawRectangle(Pens.Red, tempRect);
+				}
 			}
 		}
 
+		// States Controls
 		private void SelectImage_Click(object sender, EventArgs e)
 		{
-			if (mode == HeartMode.None)
+			if (MainImage.Image != null)
 			{
-				MainImage.MouseDown += MainImage_MouseDown;
-				MainImage.MouseMove += MainImage_MouseMove;
-				MainImage.Paint += MainImage_Paint;
-				mode = HeartMode.Select;
-			}
-			else if (mode == HeartMode.Select)
-			{
-				MainImage.MouseDown -= MainImage_MouseDown;
-				MainImage.MouseMove -= MainImage_MouseMove;
-				MainImage.Paint -= MainImage_Paint;
-				Reset();
-				mode = HeartMode.None;
+				if (mode == HeartMode.None)
+				{
+					ActiveImage(SelectImage);
+					mode = HeartMode.Select;
+				}
+				else if (mode == HeartMode.Select)
+				{
+					InactiveImage(SelectImage);
+					ResetState();
+				}
 			}
 		}
 
+		// Functions
 		private void Scan_Click(object sender, EventArgs e)
 		{
-			int startX = selectionRect.X;
-			int startY = selectionRect.Y;
-			int endX = selectionRect.X + selectionRect.Width;
-			int endY = selectionRect.Y + selectionRect.Height;
-			string result = SeverityDetector.detectHeartProblem(new Bitmap(MainImage.Image), startX, startY, endX, endY);
-			ResultLabel.Text = result;
+			if (MainImage.Image != null)
+			{
+				if (mode == HeartMode.Select && tempRect.Width > 0 && tempRect.Height > 0)
+				{
+					int startX = tempRect.X;
+					int startY = tempRect.Y;
+					int endX = tempRect.X + tempRect.Width;
+					int endY = tempRect.Y + tempRect.Height;
+					string result = SeverityDetector.detectHeartProblem(new Bitmap(MainImage.Image), startX, startY, endX, endY);
+					ResultLabel.Text = result;
+
+					ResetState();
+				}
+			}
 		}
 
 		private void PickerButton_Click(object sender, EventArgs e)
 		{
+			ResetState();
+
+			string initialDirectory = Paths.CreatePath("testImages");
+
 			OpenFileDialog openFileDialog1 = new OpenFileDialog();
+			openFileDialog1.InitialDirectory = initialDirectory;
 			if (openFileDialog1.ShowDialog() == DialogResult.OK)
 			{
 				Bitmap image = ImageProcessor.LoadImageWithResize(openFileDialog1.FileName);
 
-				MainImage.Image = image;
-				MainImage.Size = new Size(image.Width, image.Height);
+				SetImage(image);
 
 				Reset();
 			}
 		}
-
 	}
 }
